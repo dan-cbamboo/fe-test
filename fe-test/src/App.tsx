be94bfaac1064c17b074fed21ca9224a37c95725
Daniel Sharp <dsharpc@gmail.com>
2024-04-03 13:17:38 +0100

    Boilerplate

---
+++ fe-test/src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1>The Cbamboo Frontend Test</h1>
+      <h2 style={{ textAlign: "center"}}>The Café ranking tool</h2>
+      <div style={{ border: "solid",
+                    borderRadius: "25px",
+                    height: "60vh" }}>
+          <p style={{ textAlign: "center"}}>Your component here</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
